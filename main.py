6bea26b87d3ed65109ae7c88716cb3f7ec58754c
Melissa Ann Sorrells <melsorrells23@gmail.com>
2021-05-20 20:58:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print ('Welcome to Pypet!')
+
+name = 'Scooter'
+age = 5
+weight = 9.5
+hungry = True
+photo = '(=^o.o^=)__'
+
+print ('Hello ' + name)
+print (photo)
+
+cat = {
+    'name': 'Scooter',
+    'hungry': True,
+    'weight': 9.5,
+    'age': 5,
+    'photo': '(=^o.o^=)__',
+}
+
+print ('Hello ' + cat['name'])
+print (cat['photo'])
+
+print (cat)
